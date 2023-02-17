05854e06ab2331b8bf95f7bb8705c5f70b158166
khantlinnyan <khantlinnyan011027@gmail.com>
2023-02-17 22:12:10 +0630

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,24 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 const url = "https://api.github.com/users";
 function App() {
-  const [user, setUser] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-
-  async function getUser() {
-    const response = await fetch(url);
-    const userData = await response.json();
-    
-    setIsLoading(false);
-    if (response.status > 300) {
-      setIsError(true)
-      isLoading(false)
-    }
-    setUser(userData);
+  const [text , setText] = useState("");
+  const textRef = useRef(null);
+  function handleForm(e) {
+    e.preventDefault();
+    setText(textRef.current.value)
   }
-  useEffect(() => {
-    getUser();
-    console.log("UseEffect");
-  }, []);
-  if (isLoading) {
-    return (
-      <div>
-        <div className="loader">
-          <div className="loader-inner">
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-            <div className="loader-line-wrap">
-              <div className="loader-line"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  if(isError) {
-    return <h1 style={{textAlign: "center"}} >Opps is Error</h1>
-  }
-  return (
-    <div className="container">
-      <h1>Github users</h1>
-      <ul className="users">
-        {user.map((user) => {
-          return (
-            <ul key={user.id}>
-              <img src={user.avatar_url} alt={user.login}></img>
-              <h4>{user.login}</h4>
-            </ul>
-          );
-        })}
-      </ul>
+  return(
+    <div>
+    <h3>{text}</h3>
+      <form onSubmit={handleForm}>
+        <input type="text" ref={textRef} placeholder="Enter"></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
